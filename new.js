717fa9db4a760571cfb67f825e6e89fce088a6a9
Vladimir1251PD <VoldemarPoliakov@yande.ru>
2022-09-07 21:26:00 +0300

    ctreat class Rab

--- new.js
+++ new.js
@@ -2,3 +2,16 @@ console.log("lol")
 console.log("Oleg Kudj holop")
 console.log("the best holop")
 
+class Holop{
+    constructor (power, hieght){
+
+    }
+}
+
+
+class Rab extends Holop{
+    
+}
+
+
+const Oleg = new Rab(10, 198)
